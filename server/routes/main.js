659b12b6600da0260875e692d755d55f00fbb2e9
Alif Hassan Aunkur <50053867+DyridanTUDK@users.noreply.github.com>
2025-04-16 22:17:31 +0600

    ejs broke due to fault logic removing rounterchecker

--- server/routes/main.js
+++ server/routes/main.js
@@ -57,7 +57,7 @@ router.post("/search", async (req, res) => {
       ],
     });
 
-    res.render("search", { data, locals });
+    res.render("search", { data, locals, currentRoute: "/search" });
   } catch (error) {
     console.log(error);
   }
