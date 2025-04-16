1c9f447c083e3d65a7ca38e3ec83f9e0848e4196
Alif Hassan Aunkur <50053867+DyridanTUDK@users.noreply.github.com>
2025-04-16 21:55:43 +0600

    working around bcrypt

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../model/Post");
 const User = require("../model/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layout/admin";
